a68db9bdd94752e044c82688090b0fb56cd7abbc
Rajesh kumar <rkproject24@gmail.com>
2021-08-09 10:09:34 +0530

    Create RJTextBox.cs
    Place holder text added

---
+++ RJTextBox.cs
@@ -0,0 +1,258 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControls.RJControls
+{
+    [DefaultEvent("_TextChanged")]
+    public partial class RJTextBox : UserControl
+    {
+        #region Member Variables
+        Color waterMarkColor = Color.Gray;
+
+        private string _waterMarkText = "";
+        #endregion
+
+        //Fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+        private Color borderFocusColor = Color.HotPink;
+        private bool isFocused = false;
+
+        //Constructor
+        public RJTextBox()
+        {
+            InitializeComponent();
+        }
+       
+        public string WaterMarkText
+        {
+            get
+            {
+                return this._waterMarkText;
+            }
+            set
+            {
+                this._waterMarkText = value;
+                GetSetWatermark();
+            }
+        }
+
+      
+        //Events
+        public event EventHandler _TextChanged;
+
+        //Properties
+        [Category("RJ Code Advance")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+        [Category("RJ Code Advance")]
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("RJ Code Advance")]
+        public bool UnderlinedStyle
+        {
+            get { return underlinedStyle; }
+            set
+            {
+                underlinedStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("RJ Code Advance")]
+        public bool PasswordChar
+        {
+            get { return textBox1.UseSystemPasswordChar; }
+            set { textBox1.UseSystemPasswordChar = value; }
+        }
+
+        [Category("RJ Code Advance")]
+        public bool Multiline
+        {
+            get { return textBox1.Multiline; }
+            set { textBox1.Multiline = value; }
+        }
+
+        [Category("RJ Code Advance")]
+        public override Color BackColor
+        {
+            get { return base.BackColor; }
+            set
+            {
+                base.BackColor = value;
+                textBox1.BackColor = value;
+            }
+        }
+
+        [Category("RJ Code Advance")]
+        public override Color ForeColor
+        {
+            get { return base.ForeColor; }
+            set
+            {
+                base.ForeColor = value;
+                textBox1.ForeColor = value;
+            }
+        }
+
+        [Category("RJ Code Advance")]
+        public override Font Font
+        {
+            get { return base.Font; }
+            set
+            {
+                base.Font = value;
+                textBox1.Font = value;
+                if (this.DesignMode)
+                    UpdateControlHeight();
+            }
+        }
+
+        [Category("RJ Code Advance")]
+        public string Texts
+        {
+            get { return textBox1.Text.Trim() == WaterMarkText.Trim() ? "" :textBox1.Text;}
+            set { textBox1.Text = value; }
+        }
+
+        [Category("RJ Code Advance")]
+        public Color BorderFocusColor
+        {
+            get { return borderFocusColor; }
+            set { borderFocusColor = value; }
+        }
+
+        //Overridden methods
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            //Draw border
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+                if (isFocused) penBorder.Color = borderFocusColor;
+
+                if (underlinedStyle) //Line Style
+                    graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                else //Normal Style
+                    graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e); 
+            this.Invalidate();
+            if (this.DesignMode)
+            {
+                UpdateControlHeight();
+             } 
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        //Private methods
+        private void UpdateControlHeight()
+        {
+            if (textBox1.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new Size(0, txtHeight);
+                textBox1.Multiline = false;
+
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+
+        //TextBox events
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (_TextChanged != null)
+                _TextChanged.Invoke(sender, e);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            this.OnClick(e);
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.OnMouseEnter(e);
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.OnMouseLeave(e);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.OnKeyPress(e);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            isFocused = true;
+            this.Invalidate();
+            if (string.IsNullOrEmpty(this.Texts) || this.Texts == WaterMarkText)
+            {
+                this.ForeColor = Color.Black;
+                this.Texts = string.Empty;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            isFocused = false;
+            this.Invalidate();
+            GetSetWatermark();
+        }
+        private void GetSetWatermark()
+        {
+            if (string.IsNullOrEmpty(this.Texts) )
+            {
+                this.ForeColor = Color.Gray;
+                this.Texts = this.WaterMarkText;
+            }
+            else
+            {
+
+                this.ForeColor = Color.Black;
+
+            }
+        }
+        
+    }
+}
